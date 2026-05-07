d47e9d07572ddff5bb23b93bdb407f61fdf6966b
chengunkier <inabamicheal97@gmail.com>
2026-05-07 16:11:41 +0300

    completed exercise 3.7: phonebook backend step7

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const http = require('http')      
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
 
+app.use(morgan('tiny'))
 
 
 let persons = [
@@ -35,46 +37,30 @@ app.get('/', (request, response)=> {
 
 app.get('/api/persons',(request, response)=> {
     response.json(persons)
-})
+}) 
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response) => { 
     const body = request.body
-
     if (!body.name || !body.number) {
-        return response.status(400).json({
-            error: 'name or number is missing'
+        return response.status(400).json({ 
+            error: 'name or number is missing' 
         })
     }
-
-    const nameExists = persons.find(
-        person => person.name.toLowerCase() === body.name.toLowerCase()
-    )
-
+    const nameExists = persons.find(person => person.name === body.name)
     if (nameExists) {
-        return response.status(400).json({
-            error: 'name must be unique'
-        })
+        return response.status(400).json({error: 'name must be unique'})
     }
 
-    const generateId = () => {
-        if (persons.length === 0) {
-            return 1
-        }
-
-        const maxId = Math.max(...persons.map(person => person.id))
-        return maxId + 1
-    }
-
+    const id = Math.floor(Math.random() * 1000).toString()
     const person = {
-        id: generateId(),
+        id: id,
         name: body.name,
         number: body.number
     }
+    persons.push(person)
+    response.json(persons)
+}) 
 
-     persons.push(person)
-     response.json(persons)
- })
-  
 const PORT = 3000
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
