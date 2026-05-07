01e12253e5c1e81b4167ae2a1f0c5d4541452a94
chengunkier <inabamicheal97@gmail.com>
2026-05-07 12:21:47 +0300

    completed exercise 3.2 phonebook backend step2

--- index.js
+++ index.js
@@ -36,6 +36,15 @@ app.get('/', (request, response)=> {
 app.get('/api/persons',(request, response)=> {
     response.json(persons)
 })
+
+app.get('/info',(request, response)=> {
+    const count = persons.length
+    const time = new Date()
+    response.send(`
+        <p>Phonebook has info for ${count} people</p>
+        <p>${time}</p>
+        `)
+})
   
 
 const PORT = 3000
