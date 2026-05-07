553b2946dee386f7f50363b5824dd54b8c07f894
chengunkier <inabamicheal97@gmail.com>
2026-05-07 12:14:33 +0300

    completed exercise 3.1 phonebook backend step1

---
+++ index.js
@@ -0,0 +1,43 @@
+const http = require('http')      
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+
+
+let persons = [
+    {
+        id: "1",
+        name: "Arto Hellas",
+        number: "040-123456"
+    },
+    {
+        id: "2",
+        name: "Ada Lovelace",
+        number: "39-44-5323523"
+    },
+    {
+        id: "3",
+        name: "Dan abramov",
+        number: "8905789483"
+    },
+    {
+        id: "4",
+        name: "Mary Poppendieck",
+        number: "8786573208"
+    }
+]
+
+app.get('/', (request, response)=> {
+    response.send('<h1>The Project</h1>')
+})
+
+app.get('/api/persons',(request, response)=> {
+    response.json(persons)
+})
+  
+
+const PORT = 3000
+app.listen(PORT)
+console.log(`Server running on port ${PORT}`)
